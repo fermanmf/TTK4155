927522175788986145eabe8147d5d50a54e31a98
Vetle Skifjeld Rasmussen <vetlesr@ntnu.no>
2021-08-31 11:02:48 +0200

    first

---
+++ ATmega162/ATmega162/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATmega162.c
+ *
+ * Created: 31.08.2021 10:55:40
+ * Author : vetlesr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
