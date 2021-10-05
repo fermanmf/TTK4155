caee604d715f5945b527738fba113a0fc326e58d
Vetle Rasmussen <vetlesr@stud.ntnu.no>
2021-10-05 12:25:14 +0200

    added node2

---
+++ Node2/Node2/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Device_Startup\startup_sam3xa.c
+
+Device_Startup\system_sam3xa.c
+
+led.c
+
+main.c
+
+printf-stdarg.c
+
+uart.c
+
---
+++ Node2/Node2/main.c
@@ -0,0 +1,23 @@
+/*
+ * Node2.c
+ *
+ * Created: 05.10.2021 09:30:19
+ * Author : vetlesr
+ */ 
+
+
+#include "sam.h"
+#include "led.h"
+
+
+int main(void)
+{
+    SystemInit();
+	led_init();
+	led_set(led_1, true);
+	
+	
+
+    
+	
+}
