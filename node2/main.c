460da96d707f0f42ffba59f627fd7131924e1150
vetle <vetleras@gmail.com>
2021-10-30 11:50:25 +0200

    set up node2 project

--- node2/main.c
+++ node2/main.c
@@ -1,38 +1,18 @@
-/*
- * Node2.c
- *
- * Created: 05.10.2021 15:00:46
- * Author : vetlesr
- */ 
-
-
 #include "sam.h"
 #include "uart.h"
 #include "printf-stdarg.h"
-#include "can_controller.h"
-#include "dac.h"
-#include "adc.h"
-#include "pwm.h"
+#include "can.h"
 
 
 int main(void)
 {
+    /* Initialize the SAM system */
     SystemInit();
-	WDT->WDT_MR = WDT_MR_WDDIS;
-	configure_uart();
-	printf("Start\n\r");
-	can_init_def_tx_rx_mb(0x00143156);
-	
-	dac_test(1);
-	adc_init();
-	pwm_init(50);
-	pwm_set(0.5);
-	
-	
-	while(1) {
-		//printf("%u\n", IR[0]);
-	}
-	
+	WDT->WDT_MR = WDT_MR_WDDIS; //disable watchdog
+
+    configure_uart();   
+	 
+    printf("Start\n\r");  
 
     printf("Terminated\n\r");
 }
