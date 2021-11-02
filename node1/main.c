9367b12c30162eeffeef1cde7ef8a4db5d535cb1
vetleras <vetleras@gmail.com>
2021-11-02 10:54:05 +0100

    Ready for testing in node2/main.c

--- node1/main.c
+++ node1/main.c
@@ -3,35 +3,27 @@
 
 #include "uart.h"
 #include "panic.h"
-#include "em.h"
+#include "can.h"
 
+
+void setup(){
+	can_init();	
+}
+
+void _main(){
+	can_send_empty(0xFF);
+	
+}
 
 int main(){
 	MCUCR |= 1 << SRE; // enable external memory
 	SFIOR |= 1 << XMM2; // release JTAG pins from external memory address pins
-	
 	uart_init(9600);
-	em_init();
 	
-	while(1) {
-		EmEvent event = em_get_event();
-		switch (event.type) {
-			case EmJoystickPressed: 
-				break;
-				
-			case EmJoystickReleased:
-				break;
-				
-			case EmJoystickXDirectionChanged:
-				break;
-				
-			case EmJoystickYDirectionChanged:
-				break;
-			
-			default:
-				panic();
-				break;
-		}
-	}	
-	printf("Terminated\n");	
+	printf("Setting up\n\r");
+	setup();
+	printf("Done setting up. Starting main\n\r");
+	_main();
+	printf("Main is done\n\r");
 }
+
