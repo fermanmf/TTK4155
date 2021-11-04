2b1c3a1304140074cce829b9a7db71c3434bd28d
vetleras <vetleras@gmail.com>
2021-11-04 17:01:15 +0100

    Adc done

--- node1/adc.c
+++ node1/adc.c
@@ -12,7 +12,6 @@ void adc_init() {
 	OCR0 = 0; // Output compare register at 0, i.e. ck = MCK/2
 	TCCR0 = (1 << WGM00) | (1 << COM01) | (1 << WGM01) | (1 << CS00); // fast PWM mode, toggle on compare match, no prescaling
 	
-	
 	// Timer interrupt
 	OCR1A = 480; // Output compare register at 480, i.e. 10 Hz interrupt frequency with prescalar 1024
 	TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10); // CTC, 1024 prescaling
@@ -21,18 +20,10 @@ void adc_init() {
 
 ISR(TIMER1_COMPA_vect) {
 	*ADC = 0;	
-	while (!(PINE & (1 << PINE0)));	
-	(*adc_reading_received_cb)(*ADC, *ADC, *ADC, *ADC);
+	while (!(PINE & (1 << PINE0)));
+	const uint8_t v1 = *ADC;
+	const uint8_t v2 = *ADC;
+	const uint8_t v3 = *ADC;
+	const uint8_t v4 = *ADC;
+	(*adc_reading_received_cb)(v1, v2, v3, v4);
 }
-
-/*
-void adc_read(void (*callback)(uint8_t[4])) {
-	volatile uint8_t *adc = (uint8_t*) 0x1400;
-	adc[0] = 0;
-	
-	_delay_us(20);	
-	for (int i = 0; i<4; i++) {
-		data[i] = adc[0];
-	}
-}
-*/
