443a18ecd32ecf43246e3475df9037520a9fecd8
fermanmf <69243676+fermanmf@users.noreply.github.com>
2021-11-11 19:29:57 +0100

    Update main.c

--- node1/main.c
+++ node1/main.c
@@ -8,14 +8,16 @@
 // include other files under this
 #include "controller.h"
 #include "em.h"
-#include "display.h"
 
 
 void setup(){
 	controller_init();
 	em_init();
-	display_init();
 	sei();
+}
+
+void _main(){
+	while(1);
 }
 
 
@@ -28,29 +30,8 @@ int main(){
 	printf("Setting up\n\r");
 	setup();
 	printf("Done setting up. Starting main\n\r");
+	_main();
 	printf("Main is done\n\r");
-	menu();	
 	
-	//switch (state){
-			//case(0):
-				//break;
-			//case(mainMenu):
-				//display_menu(MainMenu);
-				//break;
-			//case(highScore):
-				//display_menu(HighscoreMenu);
-				//break;
-			//case(characterSelect):
-				//display_menu(CharacterMenu);
-				//break;
-			//case(inGame):
-				////display_menu());
-				//break;
-			//case(endOfGame):
-				//display_menu(EndMenu);
-				//break;
-			//case(replay):
-				//break;
-		//}
 }
 
