009d163fadaa521de8fbf7c66738a4151469d29a
Tobias <tobiasra@stud.ntnu.no>
2021-11-19 10:54:12 +0100

    Doxy ir.h

--- node2/ir.h
+++ node2/ir.h
@@ -1,5 +1,20 @@
+/**
+ * @file ir.h
+ * @brief IR sensor
+ * 
+ * Setting up ir sensor
+ */
+
 #pragma once
+
+/**
+ * @brief IR Contains the value that the ADC writes constantly
+ */
 
 volatile unsigned int *IR;
 
+/**
+ * @brief Initializing the ADC and starts reading this signal.
+ * @param callback Callback to call when ir beam is broken
+ */
 void ir_init(void (*callback)());
