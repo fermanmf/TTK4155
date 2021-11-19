8fee2c66bb28baaa313ca7f714be193388024965
Vetle Rasmussen <vetleras@gmail.com>
2021-11-19 10:29:59 +0100

    Documented node 1 (Vetle's parts)

--- node1/display.h
+++ node1/display.h
@@ -1,5 +1,25 @@
+/**
+ * @file
+ * @brief Display driver
+ */
 #pragma once
 
+/**
+ * @brief Initializes driver
+ */
 void display_init();
+
+/**
+ * @brief Writes string to specified line on display
+ * 
+ * @param string[in] String to be written to display. Max 16 chars 
+ * @param line[in] Line to write to. 0-7
+ */
 void display_write_line(char string[], int line);
+
+/**
+ * @brief Inverts specified line
+ * 
+ * @param line Line to invert
+ */
 void display_invert_line(int line);
